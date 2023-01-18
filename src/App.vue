6dbfaac36af7251761a07422f2e6ec6807befa77
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-18 16:14:59 +0100

    Add main components, create macro-structure header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
 
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
+  <div>
+    <header>
+      <AppHeader />
+    </header>
 
+    <main>
+      <AppMain />
+    </main>
+
+    <footer>
+      <AppFooter />
+    </footer>
+  </div>
 </template>
 
-<style>
-
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables.scss" as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <div class="header-container">
+        <!-- Qui inserirò il la barra nera che conterrà logo e nav-bar -->
+        <div class="upper-bar">
+            <div class="logo"></div>
+            <div class="nav-bar"></div>
+        </div>
+        <!-- Qui inserirò il jumbotron con background-image e il container di testo a sinisra del jumbo (va aggiunto anche il componentPlayButton) -->
+        <div class="jumbotron">
+            <div class="jumbo-text"></div>
+            <!-- Component Play Button -->
+            <div class="jumbo-details">
+                <!-- Elementi di dettaglio a destra del jumbotron -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header-container{
+    width: 100%;
+}
+</style>
