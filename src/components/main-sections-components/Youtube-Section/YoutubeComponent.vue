9c8cf9f66ffc0b7c3ee8a6349ccb592df9ec4af9
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-19 17:42:48 +0100

    Implement main youtube part of section

--- src/components/main-sections-components/Youtube-Section/YoutubeComponent.vue
+++ src/components/main-sections-components/Youtube-Section/YoutubeComponent.vue
@@ -19,6 +19,23 @@ export default {
             </div>
             <div class="youtube-infos">
                 <!-- Testo con info canale youtube e button -->
+                <h3>Tune up your workouts</h3>
+                <h1>The best health & fitness advice online</h1>
+                <div class="yt-channel">
+                    <img src="../../../assets/unnamed.jpg" alt="icon-yt" class="icon">
+                    <div class="channel-info">
+                        <p class="name-channel">ThemeFusion</p>
+                        <div class="channel-sub">
+                            <span id="red-part"><i class="fa-brands fa-youtube pe-2 fs-6"></i>Youtube</span>
+                            <span id="white-part">999+</span>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, sequi dolorem exercitationem provident cum obcaecati fugit.</p>
+                <button class="blue-btn" id="red-btn">
+                    <i class="fa-brands fa-youtube pe-2 fs-6"></i>
+                    Visit my Youtube channel
+                </button>
             </div>
         </article>
 
@@ -37,6 +54,7 @@ export default {
     .main-youtube{
         position: relative;
         display: flex;
+        justify-content: flex-end;
         height: 750px;
         .main-video{
             width: 50%;
@@ -52,6 +70,59 @@ export default {
                 top: 50%;
                 left: 50%;
                 transform: translate(-50% , -50%);
+            }
+        };
+        .youtube-infos{
+            width: 50%;
+            padding: 8rem;
+            h3{
+                color: $DodgerBlueColor;
+            };
+
+            h1{
+                font-size: 3rem;
+                margin: 2rem 0;
+            };
+            .yt-channel{
+                display: flex;
+                margin-bottom: 1.5rem;
+                img{
+                    width: 50px;
+                    height: 50px;
+                    margin-right: 1rem;
+                };
+                .name-channel{
+                    font-size: 1rem;
+                    color: $JumboColor;
+                    font-weight: 500;
+                    display: inline;
+                };
+                .channel-sub{
+                    font-weight: 500;
+                    margin-top: 0.5rem;
+
+                    #red-part{
+                        color: white;
+                        background-color: red;
+                        border: 1px solid $MonzaColor;
+                        padding: 0.2rem 0.5rem 0.3rem;
+                    };
+
+                    #white-part{
+                        color: $JumboColor;
+                        padding: 0.2rem 0.5rem 0.3rem;
+                        border: 1px solid $JumboColor;
+                    }
+                }
+            };
+            .blue-btn{
+                &#red-btn{
+                    margin-top: 1rem;
+                    background-color: red;
+                    border: none;
+                    font-size: 1.1rem;
+                    padding-top: 0.7rem;
+                }
             }
         }
     }
